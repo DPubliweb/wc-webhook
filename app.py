6dfe92a04a11cc8f3713c48d2013e4b7dac781b7
Dan Publiweb <91180870+DPubliweb@users.noreply.github.com>
2024-01-03 11:55:17 +0200

    debugging 502

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import logging
 app = Flask(__name__)
 
 # Configuration de logging
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
 # Variables d'environnement
@@ -16,28 +16,28 @@ port = os.environ.get('REDSHIFT_PORT')
 dbname = os.environ.get('REDSHIFT_DBNAME')
 user = os.environ.get('REDSHIFT_USER')
 password = os.environ.get('REDSHIFT_PASSWORD')
-woocommerce = os.environ.get('WCKEY')
+woocommerce_secret = os.environ.get('WCKEY')
 
-def verify_woocommerce_signature(request, woocommerce):
+def verify_woocommerce_signature(request, woocommerce_secret):
     received_signature = request.headers.get('X-WC-Webhook-Signature')
     request_payload = request.get_data(as_text=True)
-    generated_signature = hmac.new(woocommerce.encode(), request_payload.encode(), hashlib.sha256).hexdigest()
+    generated_signature = hmac.new(woocommerce_secret.encode(), request_payload.encode(), hashlib.sha256).hexdigest()
     return hmac.compare_digest(received_signature, generated_signature)
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
-    if not verify_woocommerce_signature(request, woocommerce):
+    if not verify_woocommerce_signature(request, woocommerce_secret):
         logger.error("Signature non valide, requête suspecte")
         return 'Signature non valide', 403
 
     try:
         # Traitement de la requête
-        logger.info("Webhook reçu :")
-        logger.info(request.json)
-        return '', 200
+        logger.debug("Webhook reçu :")
+        logger.debug(request.json)
+        return 'Webhook reçu avec succès', 200
     except Exception as e:
         logger.exception("Erreur lors du traitement du webhook: %s", e)
         return 'Erreur interne du serveur', 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
